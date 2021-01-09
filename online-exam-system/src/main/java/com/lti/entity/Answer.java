dd3cfd3b372a1b119f6f929ebf916ed557f3445b
Lenovo <Lenovo@DESKTOP-OGVKCHI>
2021-01-09 11:08:06 +0530

    answer entity class created

--- online-exam-system/src/main/java/com/lti/entity/Answer.java
+++ online-exam-system/src/main/java/com/lti/entity/Answer.java
@@ -1,10 +1,44 @@
 package com.lti.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "Answer")
 public class Answer {
 	
+	@Id
+	@GeneratedValue
+	private int answerId;
+	
+	private int reportId;
+	private int questionId;
+	private int optionChosen;
+	
+	public int getAnswerId() {
+		return answerId;
+	}
+	public void setAnswerId(int answerId) {
+		this.answerId = answerId;
+	}
+	public int getReportId() {
+		return reportId;
+	}
+	public void setReportId(int reportId) {
+		this.reportId = reportId;
+	}
+	public int getQuestionId() {
+		return questionId;
+	}
+	public void setQuestionId(int questionId) {
+		this.questionId = questionId;
+	}
+	public int getOptionChosen() {
+		return optionChosen;
+	}
+	public void setOptionChosen(int optionChosen) {
+		this.optionChosen = optionChosen;
+	}
 }
