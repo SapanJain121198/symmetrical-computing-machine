d91c98af12ecc8462e1bef982b7f767801904930
ashwik <ashwiklucky@gmail.com>
2021-01-09 13:21:06 +0530

    Password Module Generated

--- online-exam-system/src/main/java/com/lti/dto/Password.java
+++ online-exam-system/src/main/java/com/lti/dto/Password.java
@@ -1,15 +1,34 @@
 package com.lti.dto;
-import java.util.*;
-public class Password {
 
+public class Password {
 	
 	
+	public static String makePassword() {
+		String password="";
+		int length=10;
+		for(int i=0;i<length-2;i++) {
+			password=password + randomCharacter("ABCDEFGHIJKLMNOPQRSTUVWXYZ");	
+		}
+		String randomDigit=randomCharacter("0123456789");
+		password=insertAtRandom(password,randomDigit);
+		
+		String randomSymbol=randomCharacter("!@#$&*");
+		password=insertAtRandom(password,randomSymbol);
+		
+		return password;
+	}
 	
+	public static String randomCharacter(String alphabets) {
+		int n=alphabets.length();
+		int r=(int) (n*Math.random());
+		return alphabets.substring(r,r+1);
+	}
 	
-	
-	
-	
-	
-	
-	
+	public static String insertAtRandom(String str,String toInsert) {
+		int n=str.length();
+		int r=(int) ((n+1) * Math.random());
+		return str.substring(0,r) + toInsert + str.substring(r);
+		
+	}
+
 }
