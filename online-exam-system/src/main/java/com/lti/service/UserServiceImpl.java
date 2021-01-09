765b21b05ca182fea2f1f6b59aa71ac363475f4e
Sapan <sapan.jain@lntinfotech.com>
2021-01-09 15:58:38 +0530

    user service register and login implemented

--- online-exam-system/src/main/java/com/lti/service/UserServiceImpl.java
+++ online-exam-system/src/main/java/com/lti/service/UserServiceImpl.java
@@ -1,6 +1,71 @@
 package com.lti.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 
-public class UserServiceImpl {
+import com.lti.dao.RegisteredUserDao;
+import com.lti.entity.AdminProject;
+import com.lti.entity.RegisteredUser;
+import com.lti.exception.UserServiceException;
+
+public class UserServiceImpl implements UserService {
+
+	@Autowired
+	private RegisteredUserDao registeredUserDao;
+	
+	@Autowired
+	private AdminProjectDao adminProjectDao;
+
+	public int register(RegisteredUser registeredUser) {
+		if (registeredUserDao.isUserRegistered(registeredUser.getEmail()))
+			throw new UserServiceException("Student already registered");
+
+		RegisteredUser updatedUser = (RegisteredUser) registeredUserDao.save(registeredUser);
+
+		// code to send email to the customer on successful registration will be here
+
+		return updatedUser.getUserId();
+	}
+
+	public RegisteredUser Userlogin(String email, String password) {
+
+		try {
+
+			if (!registeredUserDao.isUserRegistered(email))
+				throw new UserServiceException("Customer not registered!");
+
+			int id = registeredUserDao.findByEmailAndPassword(email, password);
+
+			RegisteredUser registeredUser = registeredUserDao.fetchByKey(RegisteredUser.class, id);
+
+			return registeredUser;
+
+		}
+
+		catch (EmptyResultDataAccessException e) {
+			throw new UserServiceException("Incorrect email/password");
+		}
+	}
+
+	public AdminProject adminlogin(int id , String password) {
+
+		try {
+
+			if (!AdminProjectDao.isAdminRegistered(id))
+				throw new UserServiceException("Admin not registered!");
+
+			int adminId = AdminProjectDao.findByIdAndPassword(id, password);
+
+			AdminProject adminProject = adminProjectDao.fetchByKey(AdminProject.class, adminId);
+
+			return adminProject;
+
+		}
+
+		catch (EmptyResultDataAccessException e) {
+			throw new UserServiceException("Incorrect email/password");
+		}
+
+	}
 
 }
