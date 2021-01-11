b46898364bbbbcf86e57e4710816a12697cad574
Sapan <sapan.jain@lntinfotech.com>
2021-01-11 16:29:12 +0530

    random import to synchronise

--- online-exam-system/src/main/java/com/lti/entity/AdminProject.java
+++ online-exam-system/src/main/java/com/lti/entity/AdminProject.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.util.*;
 
 @Entity
 @Table(name = "Admin_Project")
