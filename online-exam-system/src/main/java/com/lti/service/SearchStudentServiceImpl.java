95bc55dfc68b34ee77673b393e46cb53608400e5
ashwik <ashwiklucky@gmail.com>
2021-01-12 10:44:04 +0530

    bug fix

--- online-exam-system/src/main/java/com/lti/service/SearchStudentServiceImpl.java
+++ online-exam-system/src/main/java/com/lti/service/SearchStudentServiceImpl.java
@@ -3,10 +3,14 @@ package com.lti.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.lti.dao.SearchStudentDao;
 import com.lti.entity.RegisteredUser;
 
+@Service
+@Transactional
 public class SearchStudentServiceImpl implements SearchStudentService {
 	
 	@Autowired
