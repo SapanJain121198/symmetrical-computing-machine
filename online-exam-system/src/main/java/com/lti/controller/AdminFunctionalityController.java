70916b0108184aaddea2862ee1c14e8f768d5956
Vallari Chitnis <chitnisvallari@gmail.com>
2021-01-12 12:20:31 +0530

    committed addquestion fixed bug..

--- online-exam-system/src/main/java/com/lti/controller/AdminFunctionalityController.java
+++ online-exam-system/src/main/java/com/lti/controller/AdminFunctionalityController.java
@@ -2,7 +2,8 @@ package com.lti.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lti.entity.Question;
@@ -17,8 +18,8 @@ public class AdminFunctionalityController {
 
 	
 	// public void addQuestion(@RequestParam("subjectName")String subjectName, @RequestParam("questionDesc") String )
-	@PostMapping("/addquestion")
-	public void receiveQuestion(Question question) {
+	@GetMapping("/addquestion")
+	public void receiveQuestion(@RequestBody Question question) {
 
 		adminFunctionalityService.addQuestion(question);
 	}
--- online-exam-system/src/main/java/com/lti/service/AdminFunctionalityServiceImpl.java
+++ online-exam-system/src/main/java/com/lti/service/AdminFunctionalityServiceImpl.java
@@ -2,6 +2,7 @@ package com.lti.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.lti.dao.AdminFunctionalityDao;
 import com.lti.entity.Question;
@@ -13,6 +14,7 @@ public class AdminFunctionalityServiceImpl implements AdminFunctionalityService 
 	private AdminFunctionalityDao adminFunctionalityDao;
 	
 	@Override
+	@Transactional
 	public void addQuestion(Question question) {
 		
 		 adminFunctionalityDao.save(question);
